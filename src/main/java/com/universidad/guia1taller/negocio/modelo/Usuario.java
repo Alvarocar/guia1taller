7b85e699c02c9229877f3f0a9bf3dd423e17a9fc
Brayan Suarez <brayansuarez123@gmail.com>
2020-08-12 00:43:24 -0500

    añadiendo la clase usuario

---
+++ src/main/java/com/universidad/guia1taller/negocio/modelo/Usuario.java
@@ -0,0 +1,33 @@
+
+package com.universidad.guia1taller.negocio.modelo;
+
+/**
+ *
+ * @author DELL
+ */
+public class Usuario {
+    private String Nombre;
+    private String Contrasena;
+    
+    public Usuario(String Nombre,String Contrasena){
+        this.Nombre = Nombre;
+        this.Contrasena = Contrasena;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getContrasena() {
+        return Contrasena;
+    }
+
+    public void setContrasena(String Contrasena) {
+        this.Contrasena = Contrasena;
+    }
+    
+}
